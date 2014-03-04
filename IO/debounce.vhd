b1e7e21074f4f5cdb93bd31cf4c3f0be61190433
nicolas.primeau@gmail.com <nprimeau@localhost.localdomain>
2014-03-04 00:44:40 -0500

    changes to debounce.vhd

--- IO/debounce.vhd
+++ IO/debounce.vhd
@@ -1,8 +1,18 @@
+--Author Nicolas Primeau
+--
+--Component : Debounce
+--
+--Description: A component that debounces inputs from physical devices that are 
+-- susceptible to debouncing. The generic is a constant that needs to be set.
+-- It depends on the clock used. By default it's 2000000 which gives us 20 ms on
+-- a 100 MHz clock.
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity debounce is
+generic(timeOut: integer:=2000000);
 port(
 	input: in std_logic;
 	clk: in std_logic;
@@ -14,8 +24,6 @@ architecture Behavioral of debounce is
 begin
 
 count: process(input,clk)
-variable timeOut: integer:=2000000; -- 20 ms for a 100 Mhz clock, Change as need for 
-                                    -- your specific board!
 variable count: integer:=0;
 variable inputCapture: std_logic;
 begin
